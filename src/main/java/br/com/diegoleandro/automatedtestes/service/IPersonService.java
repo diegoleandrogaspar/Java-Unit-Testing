e014e46d541ba41d2ba60f81940d0decaf592653
diegoleandrogaspar <diegolgaspar000@gmail.com>
2024-01-08 21:22:12 -0300

    create method createPerson

--- src/main/java/br/com/diegoleandro/automatedtestes/service/IPersonService.java
+++ src/main/java/br/com/diegoleandro/automatedtestes/service/IPersonService.java
@@ -1,4 +1,7 @@
 package br.com.diegoleandro.automatedtestes.service;
 
+import br.com.diegoleandro.automatedtestes.model.Person;
+
 public interface IPersonService {
+    Person createPerson(Person person);
 }
--- src/test/java/br/com/diegoleandro/automatedtestes/tdd/PersonServiceTest.java
+++ src/test/java/br/com/diegoleandro/automatedtestes/tdd/PersonServiceTest.java
@@ -1,18 +1,36 @@
 package br.com.diegoleandro.automatedtestes.tdd;
 
 
+import br.com.diegoleandro.automatedtestes.model.Person;
 import br.com.diegoleandro.automatedtestes.service.IPersonService;
 import br.com.diegoleandro.automatedtestes.service.PersonService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PersonServiceTest {
 
     @DisplayName("When Create a Person with Success Should Return a Person Object")
     @Test
     void testCreatePerson_WhenSucess_ShouldReturnPersonObject() {
-
+        // Given
         IPersonService service = new PersonService();
+        Person person = new Person(
+                "diego",
+                "leandro",
+                "diego@dev.com.br",
+                "Rio de Janeiro - BR",
+                "Male"
+        );
+
+        // When
+        Person actual = service.createPerson(person);
+
+        // Then / Assert
+
+        assertNotNull(actual, () -> "The createPerson() should not have returned null! ");
 
 
     }
