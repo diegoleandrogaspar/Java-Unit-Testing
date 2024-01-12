8e9be006df3d3844fb379d5be33110095f84464e
diegoleandrogaspar <diegolgaspar000@gmail.com>
2024-01-12 18:32:55 -0300

    created API

--- src/main/java/br/com/diegoleandro/automatedtestes/tdd/service/IPersonService.java
+++
@@ -1,7 +0,0 @@
-package br.com.diegoleandro.automatedtestes.tdd.service;
-
-import br.com.diegoleandro.automatedtestes.tdd.model.Person;
-
-public interface IPersonService {
-    Person createPerson(Person person);
-}
