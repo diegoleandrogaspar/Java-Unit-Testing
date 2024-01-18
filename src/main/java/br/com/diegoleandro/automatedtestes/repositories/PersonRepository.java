4aa533b866a5cbecfc5d90993f37740a682d8c9b
diegoleandrogaspar <diegolgaspar000@gmail.com>
2024-01-18 09:10:45 -0300

    Refatorando os Testes Unitários JUnit na camada de Repositórios

--- src/main/java/br/com/diegoleandro/automatedtestes/repositories/PersonRepository.java
+++ src/main/java/br/com/diegoleandro/automatedtestes/repositories/PersonRepository.java
@@ -2,8 +2,39 @@ package br.com.diegoleandro.automatedtestes.repositories;
 
 import br.com.diegoleandro.automatedtestes.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.Optional;
+
+
 public interface PersonRepository extends JpaRepository<Person, Long> {
+
+    Optional<Person> findByEmail(String email);
+
+    // Define custom query using JPQL with index parameters
+    @Query("select p from Person p where p.firstName = ?1 and p.lastName = ?2")
+    Person findbyJPQL(String firstName, String lastName);
+
+
+    //custom query using JPQL with named parameters
+    @Query("select p from Person p where p.firstName =:firstName and p.lastName =:lastName")
+    Person findbyJPQLNamedParameters(
+            @Param("firstName") String firstName,
+            @Param("lastName") String lastName);
+
+
+    //custom query using Native SQL with index parameters
+    @Query(value = "select * from person p where p.first_name =?1 and p.last_name =?2", nativeQuery = true)
+    Person findbyNativeSQL(String firstName, String lastName);
+
+
+    //custom query using Native SQL with index named parameters
+    @Query(value = "select * from person p where p.first_name =:firstName and p.last_name =:lastName", nativeQuery = true)
+    Person findbyNativeSQLwithNamedParameters(
+            @Param("firstName") String firstName,
+            @Param("lastName") String lastName);
+
+
 }
--- src/test/java/br/com/diegoleandro/automatedtestes/repositories/PersonRepositoryTest.java
+++ src/test/java/br/com/diegoleandro/automatedtestes/repositories/PersonRepositoryTest.java
@@ -2,12 +2,14 @@ package br.com.diegoleandro.automatedtestes.repositories;
 
 
 import br.com.diegoleandro.automatedtestes.model.Person;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,13 +19,21 @@ public class PersonRepositoryTest  {
     @Autowired
     private PersonRepository personRepository;
 
+
+    private Person person0;
+
+    @BeforeEach
+    public void setup() {
+      //Given
+     person0 = new Person("Diego", "Leandro",  "Rio de Janeiro - RJ - Brasil","Male",  "diego@mail.com"  );
+
+    }
+
+
+
     @DisplayName("Given person Object when save the Return Saved Person")
     @Test
     void testGivenPersonObject_whenSave_thenReturnedSavedPerson() {
-
-     // Given
-    Person person0 = new Person("Diego", "Leandro",  "Rio de Janeiro - RJ - Brasil","Male",  "diego@mail.com"  );
-
      // When
     Person savedPerson = personRepository.save(person0);
 
@@ -38,7 +48,6 @@ public class PersonRepositoryTest  {
     void testGivenPersonList_whenFindAll_thenReturnedSPersonList() {
 
         // Given
-        Person person0 = new Person("Diego", "Leandro", "Rio de Janeiro - RJ - Brasil", "Male", "diego@mail.com");
         Person person1 = new Person("Zidane", "Leandro", "Rio de Janeiro - RJ - Brasil", "Male", "diego@mail.com");
 
         personRepository.save(person0);
@@ -59,8 +68,6 @@ public class PersonRepositoryTest  {
     @Test
     void testGivenPersonObject_whenFindById_thenReturnedSavedPersonObject() {
 
-        // Given
-        Person person0 = new Person("Diego", "Leandro",  "Rio de Janeiro - RJ - Brasil","Male",  "diego@mail.com"  );
         personRepository.save(person0);
 
         // When
@@ -74,5 +81,133 @@ public class PersonRepositoryTest  {
     }
 
 
+    @DisplayName("Given person Object when FindByEmail the Return Person Object")
+    @Test
+    void testGivenPersonObject_whenFindByEmail_thenReturnedSavedPersonObject() {
+
+        personRepository.save(person0);
+
+        // When
+        Person savedPerson = personRepository.findByEmail(person0.getEmail()).get();
+
+
+        // Then
+        assertNotNull(savedPerson);
+        assertEquals(person0.getId(), savedPerson.getId());
 
     }
+
+    @DisplayName("Given person Object when Updated Person the Return Updated Person Object")
+    @Test
+    void testGivenPersonObject_whenUpdatePerson_thenReturnedUpdatePersonObject() {
+
+        personRepository.save(person0);
+
+        // When
+        Person savedPerson = personRepository.findById(person0.getId()).get();
+        savedPerson.setFirstName("Leonardo");
+        savedPerson.setEmail("leonardo@erudio.com.br");
+
+        Person updatePerson = personRepository.save(savedPerson);
+
+
+        // Then
+        assertNotNull(updatePerson);
+        assertEquals("Leonardo", updatePerson.getFirstName());
+        assertEquals("leonardo@erudio.com.br", savedPerson.getEmail());
+    }
+
+    @DisplayName("Given person Object when Delete the Remove Person")
+    @Test
+    void testGivenPersonObject_whenDelete_thenRemovePerson() {
+
+        personRepository.save(person0);
+
+        // When
+        personRepository.deleteById(person0.getId());
+        Optional<Person> personOptional = personRepository.findById(person0.getId());
+
+        // Then
+        assertTrue(personOptional.isEmpty());
+
+    }
+
+    @DisplayName("Given firstName and lastName when findJPQL the Return Person Object")
+    @Test
+    void testGivenFirstNameAndLastName_whenFindJPQL_thenReturnedSavedPersonObject() {
+
+        personRepository.save(person0);
+        String firstName = "Diego";
+        String lastName = "Leandro";
+
+
+        // When
+        Person savedPerson = personRepository.findbyJPQL(firstName, lastName);
+
+
+        // Then
+        assertNotNull(savedPerson);
+        assertEquals(firstName, savedPerson.getFirstName());
+        assertEquals(lastName, savedPerson.getLastName());
+    }
+
+    @DisplayName("Given firstName and lastName when findJPQLNamedParameters the Return Person Object")
+    @Test
+    void testGivenFirstNameAndLastName_whenFindJPQLNamedParameters_thenReturnedSavedPersonObject() {
+
+        personRepository.save(person0);
+        String firstName = "Diego";
+        String lastName = "Leandro";
+
+
+        // When
+        Person savedPerson = personRepository.findbyJPQLNamedParameters(firstName, lastName);
+
+
+        // Then
+        assertNotNull(savedPerson);
+        assertEquals(firstName, savedPerson.getFirstName());
+        assertEquals(lastName, savedPerson.getLastName());
+    }
+
+
+    @DisplayName("Given firstName and lastName when findByNativeSQL the Return Person Object")
+    @Test
+    void testGivenFirstNameAndLastName_whenFindByNativeSQL_thenReturnedSavedPersonObject() {
+
+        personRepository.save(person0);
+
+        String firstName = "Diego";
+        String lastName = "Leandro";
+
+
+        // When
+        Person savedPerson = personRepository.findbyNativeSQL(firstName, lastName);
+
+
+        // Then
+        assertNotNull(savedPerson);
+        assertEquals(firstName, savedPerson.getFirstName());
+        assertEquals(lastName, savedPerson.getLastName());
+    }
+
+
+    @DisplayName("Given firstName and lastName when findByNativeSQLwithNamedParameters the Return Person Object")
+    @Test
+    void testGivenFirstNameAndLastName_whenFindByNativeSQLwithNamedParameters_thenReturnedSavedPersonObject() {
+
+        personRepository.save(person0);
+
+        String firstName = "Diego";
+        String lastName = "Leandro";
+
+        // When
+        Person savedPerson = personRepository.findbyNativeSQLwithNamedParameters(firstName, lastName);
+
+        // Then
+        assertNotNull(savedPerson);
+        assertEquals(firstName, savedPerson.getFirstName());
+        assertEquals(lastName, savedPerson.getLastName());
+    }
+
+}
